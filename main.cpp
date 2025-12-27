9bccf03f5eba2ea7f86c1d8664f29b18e17cf6f6
sridharan-kannan-06 <sridharank1805@gmail.com>
2025-12-27 17:14:15 +0530

    Wrapped up the main function to call the game board function for the gui

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
+#include <gtkmm/application.h>
+#include "game_window.hpp"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char* argv[]) {
+    auto app = Gtk::Application::create(argc, argv, "org.slant.senguthu");
+    game_window window;
+    return app->run(window);
 }
+
+// COMPILE COMMAND (Run this in MSYS2/Terminal):
+// g++ -std=c++17 main.cpp level_data.cpp graph.cpp game_board.cpp greedy_cpu.cpp game_window.cpp -o slant.exe `pkg-config gtkmm-3.0 --cflags --libs`
+// ./slant.exe
